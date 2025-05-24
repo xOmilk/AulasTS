b78e38d0a3387d46d3e38368971d457ddbe38f23
xOmilk <leite46052@gmail.com>
2025-05-24 16:58:08 -0300

    Type Annotation

---
+++ src/typeanotation.ts
@@ -0,0 +1,31 @@
+
+let nome: string = 'Antonio';
+let idade: number = 21;
+let adulto: boolean = true;
+
+let linguagensArray: string[] = [
+
+    "java",
+    'C',
+    'javascript',
+    'typescript',
+    'php'
+
+]
+
+let pessoa: { nome: string, idade: number, adulto?: boolean } = {
+
+    nome,
+    idade,
+    adulto,
+}
+
+function soma(valor1: number, valor2: number) {
+    return valor1 + valor2;
+}
+
+const result = soma(1, 2);
+
+
+
+
