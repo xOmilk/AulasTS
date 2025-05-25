437f7cdb75b92519fb9c446ac5f67987d605e903
xOmilk <leite46052@gmail.com>
2025-05-24 23:54:30 -0300

    Criação de proprios tipos alias

---
+++ src/type_alias.ts
@@ -0,0 +1,30 @@
+
+//Definindo novos tipos criados por mim
+type Idade = number;
+type Pessoa = {
+    nome: string;
+    idade: Idade;
+    salario: number;
+    corPreferida?: string;
+}
+type CorRGB = 'Vermelho' | 'Verde' | 'Azul';
+type CorCMYK = 'Ciano' | 'Magenta' | 'Amarelo' | 'Preto';
+type CorPreferida = CorRGB | CorCMYK;
+
+//Definindo um objeto literal
+let pessoaLiteral: Pessoa = {
+    nome: "Fulano",
+    idade: 21,
+    salario: 3_000,
+}
+
+function setCorPreferida(pessoa: Pessoa, cor: CorPreferida): Pessoa {
+    return { ...pessoa, corPreferida: cor }
+}
+console.log(pessoaLiteral);
+
+pessoaLiteral = setCorPreferida(pessoaLiteral, 'Verde');
+console.log(pessoaLiteral);
+
+
+
