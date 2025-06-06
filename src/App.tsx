868af0551d6649f4808fedb56a63ce641d3153d2
xOmilk <leite46052@gmail.com>
2025-06-05 22:23:08 -0300

    Testando animations no css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import "./App.css";
+import { Button } from "./components/Button";
 import { ReducerLearning } from "./pages/ReducerLearning";
 import { SimpleForm } from "./pages/SimpleForm";
 
 export function App() {
-	return <ReducerLearning />;
+	return <Button />;
 }
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,11 @@
+import './styles.module.css'
+
+export function Button() {
+	return (
+		<>
+			<h1>Teste</h1>
+
+			<button>Passe o mouse</button>
+		</>
+	);
+}
